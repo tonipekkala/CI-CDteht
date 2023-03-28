d7ff888cb77b583fd34df6edeb5df46cecab10a5
tonipek <tonipek@metropolia.fi>
2023-03-28 12:58:19 +0300

    testii

--- index.js
+++ index.js
@@ -1,2 +1,40 @@
-var msg = "Hello";
-console.log(msg);
+const http = require('http');
+const qs = require('querystring');
+const calculator = require('./calculator');
+const port = 3000;
+const host = '127.0.0.1';
+
+const server = http.createServer((request, response) => {
+  console.dir(request.param);
+
+  if (request.method == 'POST') {
+    console.log('POST');
+    let body = '';
+    request.on('data', (data) => {
+      body += data;
+    });
+
+    request.on('end', () => {
+      const post = qs.parse(body);
+      const numbers = post.numbers;
+      const result = calculator.add(numbers);
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.end('Result: ' + result);
+    });
+  } else {
+    const html = `
+            <html>
+                <body>
+                    <form method="post" action="http://localhost:3000">Numbers: 
+                        <input type="text" name="numbers" />
+                        <input type="submit" value="Add" />
+                    </form>
+                </body>
+            </html>`;
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end(html);
+  }
+});
+
+server.listen(port, host);
+console.log(`Listening at http://${host}:${port}`);
